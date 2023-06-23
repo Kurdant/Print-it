dc481484ed5222596ad5773bca0584cd2dd41140
Hugo <hugo@shenron.fr>
2023-06-23 22:28:17 +0200

    new commit final code

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,57 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+const Fleche_gauche = document.querySelector(".arrow_left");
+const Fleche_droite = document.querySelector(".arrow_right");
+const imageContainer = document.querySelector(".banner-img");
+const tagLineContainer = document.querySelector("#banner p");
+const dots = document.querySelectorAll(".dot");
+
+Fleche_gauche.addEventListener("click", () => changeSlide("gauche"));
+Fleche_droite.addEventListener("click", () => changeSlide("droite"));
+
+let count = 0;
+
+function changeSlide(direction) {
+
+	dots.forEach(dot => dot.classList.remove("dot_selected"));
+
+	if (direction === "gauche") {
+		count--;
+		if (count < 0) {
+			count = slides.length - 1;
+		}
+	} else {
+		count++;
+		if (count === slides.length) {
+			count = 0;
+		}
+	}
+
+	const slide = slides[count];
+	imageContainer.src = slide.image;
+	tagLineContainer.innerHTML = slide.tagLine;
+
+	dots[count].classList.add("dot_selected");
+}
+
+
+
+
+		
